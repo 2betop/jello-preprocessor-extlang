2c6d8a356d6571f366446d3ce474728abfb1e2ff
lily-zhangying <zhangying3210@gmail.com>
2014-05-04 19:30:18 +0800

    update

--- test/ut/preprocessor-extVelocity.js
+++ test/ut/preprocessor-extVelocity.js
@@ -1,1 +1,13 @@
+var expect = require('chai').expect,
+    fs = require('fs'),
+    ext_v = require('../../index.js');
 
+describe('compile', funcion(){
+    
+    it('', function(){
+        var content = fs.readFileSync(__dirname + '/file/index.vm');
+        var target = fs.readFileSync(__dirname + '/file/target.vm');
+        expect(content).to.equal(target);
+
+    });
+});
